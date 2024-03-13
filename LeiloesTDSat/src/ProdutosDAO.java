87a8094d92253ee9f1fd11da7075ab2fab4bba93
JosyBarreto -Projeto 1https://github.com/JosyBarreto/Projeto-Leiloes-TDSat <josi.barreto1@gmail.com>
2024-03-13 19:06:35 -0300

    Remoção de comentarios e organização de  códigos

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
@@ -20,35 +11,33 @@ public class ProdutosDAO {
     static boolean atualizarProduto(ProdutosDTO produto) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public boolean cadastrarProduto (ProdutosDTO produto){
-        
-        
+
+    public boolean cadastrarProduto(ProdutosDTO produto) {
+
         conn = new conectaDAO().connectDB();
-        
-        
+
         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
-        
+
         try {
             PreparedStatement prep = conn.prepareStatement(sql);
             prep.setString(1, produto.getNome());
             prep.setInt(2, produto.getValor());
             prep.setString(3, produto.getStatus());
-            
+
             int rowsAffected = prep.executeUpdate();
-            
+
             if (rowsAffected > 0) {
                 System.out.println("Produto cadastrado com sucesso!");
                 return true;
             } else {
                 System.out.println("Nenhum produto foi cadastrado.");
                 return false;
-}
+            }
         } catch (SQLException erro) {
             System.out.println("Erro ao cadastrar produto: " + erro.getMessage());
             return false;
@@ -63,26 +52,22 @@ public class ProdutosDAO {
         }
     }
 
-       
-    
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-      ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-        
-        
+    public ArrayList<ProdutosDTO> listarProdutos() {
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
         String sql = "SELECT * FROM produtos";
-        
+
         try {
             PreparedStatement prep = conn.prepareStatement(sql);
             ResultSet rs = prep.executeQuery();
-            
-            while(rs.next()) {
+
+            while (rs.next()) {
                 ProdutosDTO produto = new ProdutosDTO();
                 produto.setId(rs.getInt("id"));
                 produto.setNome(rs.getString("nome"));
                 produto.setValor(rs.getInt("valor"));
                 produto.setStatus(rs.getString("status"));
-                
+
                 listagem.add(produto);
             }
         } catch (SQLException erro) {
@@ -96,15 +81,7 @@ public class ProdutosDAO {
                 System.out.println("Erro ao fechar conexão: " + e.getMessage());
             }
         }
-        
+
         return listagem;
     }
-}  
-       
-    
-    
-    
-    
-        
-
-
+}
