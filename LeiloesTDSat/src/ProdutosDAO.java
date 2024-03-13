5cfeeffe4efa4a88889d2759b917147e5c21931a
JosyBarreto -Projeto 1https://github.com/JosyBarreto/Projeto-Leiloes-TDSat <josi.barreto1@gmail.com>
2024-03-12 23:12:53 -0300

    Mensagem de cadastro Atualizada

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -13,7 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
 
@@ -26,21 +26,85 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public boolean cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
         
         
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        
+        try {
+            PreparedStatement prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            int rowsAffected = prep.executeUpdate();
+            
+            if (rowsAffected > 0) {
+                System.out.println("Produto cadastrado com sucesso!");
+                return true;
+            } else {
+                System.out.println("Nenhum produto foi cadastrado.");
+                return false;
+}
+        } catch (SQLException erro) {
+            System.out.println("Erro ao cadastrar produto: " + erro.getMessage());
+            return false;
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
     }
+
+       
+    
     
     public ArrayList<ProdutosDTO> listarProdutos(){
+      ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        
+        
+        String sql = "SELECT * FROM produtos";
+        
+        try {
+            PreparedStatement prep = conn.prepareStatement(sql);
+            ResultSet rs = prep.executeQuery();
+            
+            while(rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                
+                listagem.add(produto);
+            }
+        } catch (SQLException erro) {
+            System.out.println("Erro ao listar produtos: " + erro.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
         
         return listagem;
     }
+}  
+       
+    
     
     
     
         
-}
+
 
